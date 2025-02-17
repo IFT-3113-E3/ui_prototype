379593b9cba1822276947eaac6a57c3356d172df
Angelina Casanova <anghjulinacasa@gmail.com>
2025-02-17 15:22:27 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using Unity.Logging;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Log.Warning("Multiple GameManager in scene!");
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+        // Setup and initial configuration
+        // ...
+        Log.Info("Game Manager has been instantiated.");
+    }
+
+    private void Start()
+    {
+        Log.Info("Starting Game Manager");
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+This is a template for Unity projects, with sensible .gitignore defaults for Unity. This template includes an empty project configured with Unity's universal rendering pipeline.
+
+### Steps to get started
+1. Install the Unity 6 editor (6000.0.38f1)
+2. Download or clone the repository
+```bash
+git clone https://github.com/IFT-3113-E3/project-template.git
+```
+3. Open the repository folder with Unity
